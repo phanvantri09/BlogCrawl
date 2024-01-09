ef36912f609c19c2f62c77224e56697f2949a5e6
Phan Văn Trí <72957574+phanvantri09@users.noreply.github.com>
2024-01-09 16:43:13 +0700

    Update Image.php

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,21 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Product;
+use App\Models\Post;
 class Image extends Model
 {
     use HasFactory;
     protected $table = 'images';
 
     protected $fillable = [
-        'id_product',
-        'link_image',
-        'description',
+        'id_post',
+        'name',
+        'link',
         'type',
         'is_slide',
     ];
-    public function product()
+    public function Post()
     {
-        return $this->belongsTo(Product::class, 'id_product', 'id');
+        return $this->belongsTo(Post::class, 'id_product', 'id');
     }
 }
