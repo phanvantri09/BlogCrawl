a786db5621705434414f6efd09d1e4c9926a9c57
DuongVanTuan <duong.van.tuan@uruca.jp>
2024-01-10 13:10:56 +0700

    Tuan_user: format code file sidebar.html

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -2,7 +2,7 @@
     <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu"   role="button"><i class="fas fa-bars"></i></a>
+            <a class="nav-link" data-widget="pushmenu" role="button"><i class="fas fa-bars"></i></a>
         </li>
     </ul>
 
@@ -11,7 +11,8 @@
         <!-- Notifications Dropdown Menu -->
         <li class="nav-item dropdown">
             <div class="info w-100">
-                <a href="{{ route('logout') }}" class="w-100 btn btn-danger"> <i class="fas fa-sign-out-alt"></i> Đăng xuất</a>
+                <a href="{{ route('logout') }}" class="w-100 btn btn-danger"> <i class="fas fa-sign-out-alt"></i> Đăng
+                    xuất</a>
             </div>
         </li>
     </ul>
@@ -22,8 +23,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link d-flex flex-column">
-        <img src="dist/img/logo.png" alt="AdminLTE Logo" class="brand-image  elevation-3"
-            style="opacity: .8">
+        <img src="dist/img/logo.png" alt="AdminLTE Logo" class="brand-image  elevation-3" style="opacity: .8">
     </a>
 
     <!-- Sidebar -->
@@ -34,7 +34,7 @@
                 <img src="dist/img/avatar.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a   class="d-block">{{ Auth::user()->email }}</a>
+                <a class="d-block">{{ Auth::user()->email }}</a>
             </div>
         </div>
 
@@ -89,7 +89,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a   class="nav-link">
+                    <a class="nav-link">
                         <i class="fas fa-users"></i>
                         <p>
                             Người dùng
@@ -104,7 +104,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a   class="nav-link">
+                            <a class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Danh sách</p>
                             </a>
@@ -116,8 +116,7 @@
                                 </a>
                             </ul>
                             <ul>
-                                <a href="{{ route('user.list', ['type' => 3]) }}"
-                                    class="nav-link">
+                                <a href="{{ route('user.list', ['type' => 3]) }}" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>User được giới thiệu</p>
                                 </a>
@@ -128,11 +127,11 @@
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Admin</p>
                                 </a>
-                    </ul>
+                            </ul>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a   class="nav-link">
+                    <a class="nav-link">
                         <i class="fas fa-boxes"></i>
                         <p>
                             Loại bài post
@@ -153,7 +152,8 @@
                             </a>
                         </li>
                     </ul>
-                </li>            </ul>
+                </li>
+            </ul>
         </nav>
         <!-- /.sidebar-menu -->
     </div>
