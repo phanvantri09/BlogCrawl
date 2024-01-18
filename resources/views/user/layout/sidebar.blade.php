7d70b0268d7962662e5ac2abe7bf5b20fe4b2d63
kimoanh <hulk09022000@gmail.com>
2024-01-18 21:48:11 +0700

    home done - khiếu nại done

---
+++ resources/views/user/layout/sidebar.blade.php
@@ -0,0 +1,26 @@
+<nav class="nav-bar">
+    <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </div>
+    <a href="#" class="nav-branding">J.K.</a>
+
+
+    <ul class="nav-menu">
+        <li class="nav-item">
+            <a href="#" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Services</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">About</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Contact</a>
+        </li>
+    </ul>
+
+
+</nav>
